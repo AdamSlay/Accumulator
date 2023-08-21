199c20cafc51e8d80c524681b3ba8cf182dc1dd6
Adam Slay <aslay@mesonet.org>
2023-08-21 13:53:15 -0500

    refactor main() utils into utils.py

--- accumulator/main.py
+++ accumulator/main.py
@@ -1,31 +1,8 @@
-from datetime import datetime
-
-import pandas as pd
-
 from accumulator.config import ACC_DATASET_PATH, STATION_PARAMETERS, MODELS_TO_RUN
-from accumulator.models.chill import calculate_chill_hours
 from accumulator.ncdf_utils import read_ncdf, write_ncdf, combine_datasets
 from accumulator.parm import Parm
 from accumulator.portal import fetch_parm
-
-
-def set_time_stamp() -> int:
-    """
-    Calculate the number of hours since the reference date
-
-    :return: Hours since reference date as int
-    """
-    current_time = datetime.now()
-    reference_date = datetime(1990, 1, 1, 0, 0, 0)
-    new_time_stamp = (current_time - reference_date).total_seconds() // 3600  # 3600 seconds in an hour
-    return int(new_time_stamp)
-
-
-def run_selected_models(models: list, combined_data: pd.DataFrame):
-    updated_accumulation = combined_data
-    if 'utah' in models:
-        updated_accumulation = calculate_chill_hours(combined_data, 'utah')
-    return updated_accumulation
+from accumulator.utils import set_time_stamp, run_selected_models
 
 
 def main():
---
+++ accumulator/utils.py
@@ -0,0 +1,23 @@
+from accumulator.models.chill import calculate_chill_hours
+from datetime import datetime
+import pandas as pd
+
+
+def set_time_stamp() -> int:
+    """
+    Calculate the number of hours since the reference date
+
+    :return: Hours since reference date as int
+    """
+    current_time = datetime.now()
+    reference_date = datetime(1990, 1, 1, 0, 0, 0)
+    new_time_stamp = (current_time - reference_date).total_seconds() // 3600  # 3600 seconds in an hour
+    return int(new_time_stamp)
+
+
+def run_selected_models(models: list, combined_data: pd.DataFrame):
+    updated_accumulation = combined_data
+    if 'utah' in models:
+        updated_accumulation = calculate_chill_hours(combined_data, 'utah')
+    return updated_accumulation
+
