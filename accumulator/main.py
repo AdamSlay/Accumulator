89795e6c1181b4a2abdba470dbc48792949fc3c2
Adam Slay <aslay@mesonet.org>
2023-08-21 11:11:34 -0500

    update type hints

--- accumulator/main.py
+++ accumulator/main.py
@@ -6,12 +6,15 @@ from accumulator.parm import Parm
 from accumulator.portal import fetch_parm
 
 
-def set_time_stamp() -> float:
-    # Calculate the number of hours since the reference date
+def set_time_stamp() -> int:
+    """
+    Calculate the number of hours since the reference date
+    :return: Hours since reference date as float
+    """
     current_time = datetime.now()
     reference_date = datetime(1990, 1, 1, 0, 0, 0)
-    new_time_stamp = (current_time - reference_date).total_seconds() / 3600  # 3600 seconds in an hour
-    return new_time_stamp
+    new_time_stamp = (current_time - reference_date).total_seconds() // 3600  # 3600 seconds in an hour
+    return int(new_time_stamp)
 
 
 def main():
