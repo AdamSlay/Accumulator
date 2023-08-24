eb6df2994b87ea9b882c0fe281dbdb604c083fe7
Adam Slay <aslay@mesonet.org>
2023-08-24 09:48:00 -0500

    init logging

--- accumulator/main.py
+++ accumulator/main.py
@@ -1,19 +1,22 @@
+import logging
+from logging.handlers import TimedRotatingFileHandler
+
 from accumulator.ncdf_update import write_ncdf
 from accumulator.portal import fetch_parm
 from accumulator.utils import set_time_stamp, run_models
 
 
 def main():
-    # Fetch the latest tair data from DataPortal at the top of the hour for each station
-    stations_csv = fetch_parm()
+    handler = TimedRotatingFileHandler('accumulator.log', when='D', interval=30, backupCount=5)
+    logging.basicConfig(level=logging.INFO, handlers=[handler])
 
-    # Run the models
-    updated_accumulation = run_models(stations_csv)
-
-    # Set time stamp and Save accumulated hours to NetCDF4 file
-    new_time_stamp = set_time_stamp()
-    write_ncdf(updated_accumulation, new_time_stamp)
-
+    try:
+        stations_csv = fetch_parm()
+        updated_accumulation = run_models(stations_csv)
+        new_time_stamp = set_time_stamp()
+        write_ncdf(updated_accumulation, new_time_stamp)
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
 
 if __name__ == '__main__':
     main()
