ffdb13c53bab0a35914092b92c1ad07c913360b7
Adam Slay <aslay@mesonet.org>
2023-08-24 09:53:32 -0500

    init logging

--- accumulator/config.py
+++ accumulator/config.py
@@ -1,12 +1,27 @@
 import os
 import json
 
-ACC_DATASET_PATH = os.environ.get('ACC_DATASET_PATH')
-CHILL_HOURS_VAR = os.environ.get('CHILL_HOURS_VAR')
-DATAPORTAL_REQUEST_URL = os.environ.get('DATAPORTAL_REQUEST_URL')
+# Path to the NetCDF4 dataset file
+ACC_DATASET_PATH = os.environ.get('ACC_DATASET_PATH', 'default_path')
 
-models_to_run_string = os.environ.get('MODELS_TO_RUN')
-MODELS_TO_RUN = json.loads(models_to_run_string)
+# Variable name for chill hours in the NetCDF4 file
+CHILL_HOURS_VAR = os.environ.get('CHILL_HOURS_VAR', 'default_var')
 
-station_parms_string = os.environ.get('STATION_PARAMETERS')
-STATION_PARAMETERS = json.loads(station_parms_string)
+# URL for the DataPortal request
+DATAPORTAL_REQUEST_URL = os.environ.get('DATAPORTAL_REQUEST_URL', 'default_url')
+
+# List of models to run
+models_to_run_string = os.environ.get('MODELS_TO_RUN', '[]')
+try:
+    MODELS_TO_RUN = json.loads(models_to_run_string)
+except json.JSONDecodeError:
+    print(f"Invalid JSON for MODELS_TO_RUN: {models_to_run_string}")
+    MODELS_TO_RUN = []
+
+# List of station parameters
+station_parms_string = os.environ.get('STATION_PARAMETERS', '[]')
+try:
+    STATION_PARAMETERS = json.loads(station_parms_string)
+except json.JSONDecodeError:
+    print(f"Invalid JSON for STATION_PARAMETERS: {station_parms_string}")
+    STATION_PARAMETERS = []
