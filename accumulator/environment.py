b8d2e50e84a0cce3bab83e489c1b9534f5f131a7
Adam Slay <aslay@mesonet.org>
2023-08-28 13:06:33 -0500

    fix issue where UPDATE_FUNCTIONS was not caps

--- accumulator/environment.py
+++ accumulator/environment.py
@@ -14,27 +14,13 @@ DATE_TIME = now_utc.strftime('%Y-%m-%d %H:00:00')
 
 # ENVIRONMENT VARIABLES
 ACCUM_DATASET_PATH = os.environ.get('ACCUM_DATASET_PATH', 'default_path')
-
-# DataServer IP
 DATASERVER_IP = os.environ.get('DATASERVER_IP', 'default_ip')
-
-# DataServer Port
 DATASERVER_PORT = int(os.environ.get('DATASERVER_PORT', 0))
 
 # CONFIG 
 config = toml.load('etc/config/config.toml')
-
-# Variable name for chill hours in the NetCDF4 file
 CHILL_HOURS_VAR = config['settings'].get('CHILL_HOURS_VAR', 'default_var')
-
-# DataServer Dataset
 DATASERVER_DATASET = config['settings'].get('DATASERVER_DATASET', 'default_dataset')
-
-# DataServer Request Type
 DATASERVER_REQ_TYPE = config['settings'].get('DATASERVER_REQ_TYPE', 'default_request_type')
-
-# List of models to run
 MODELS_TO_RUN = config['settings'].get('MODELS_TO_RUN', [])
-
-# List of station parameters
 STATION_PARAMETERS = config['settings'].get('STATION_PARAMETERS', [])
