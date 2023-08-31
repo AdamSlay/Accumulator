a3e1ce2079e55567c6fc69bccb2d55df8e66d600
Adam Slay <aslay@mesonet.org>
2023-08-31 09:57:33 -0500

    log to stdout so that it appears in CloudWatch

--- accumulator/utils/logger.py
+++ accumulator/utils/logger.py
@@ -1,11 +1,10 @@
 import logging
-from logging.handlers import TimedRotatingFileHandler
-from accumulator.environment import LOG_LEVEL, LOG_PATH
+import sys
 
+from accumulator.environment import LOG_LEVEL
 
 def init_logging():
-    # the handler below will rotate the log file every 30 days and keep 5 backups of the log file
-    handler = TimedRotatingFileHandler(LOG_PATH, when='D', interval=30, backupCount=5)
+    handler = logging.StreamHandler(sys.stdout)
     logging.basicConfig(
         level=LOG_LEVEL,
         format='%(asctime)s|%(levelname)s|%(name)s|%(message)s',
