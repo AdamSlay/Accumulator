35e1b41d0702b491800028711bd6a599747334f0
Adam Slay <aslay@mesonet.org>
2023-09-08 14:15:51 -0500

    better test for portal.py functions

--- tests/test_portal.py
+++ tests/test_portal.py
@@ -7,62 +7,48 @@ from accumulator import portal
 
 
 def test_fetch_station_data(monkeypatch):
-    # Mock the socket connection
     mock_socket = MagicMock()
     monkeypatch.setattr("socket.socket", lambda *args, **kwargs: mock_socket)
 
-    # Mock the query
     mock_query = {"type": "test_type", "dataset": "test_dataset", "date": "test_date",
                   "variables": ["test_var1", "test_var2"]}
     monkeypatch.setattr(portal, "build_query", lambda: mock_query)
 
-    # Mock the response
     mock_response = {"success": True, "response": {"test_var1": {"data": [1, 2, 3]}, "test_var2": {"data": [4, 5, 6]}}}
     monkeypatch.setattr(portal, "receive_response", lambda x: mock_response)
 
-    # Mock the DataFrame conversion
     mock_df = pd.DataFrame(mock_response['response'])
     monkeypatch.setattr(portal, "convert_resp_to_df", lambda x: mock_df)
 
-    # Call the function
     result = portal.fetch_station_data()
 
-    # Assert the result
     pd.testing.assert_frame_equal(result, mock_df)
 
 
 def test_build_query():
-    # Call the function with some test inputs
     result = portal.build_query("test_type", "test_dataset", "test_date", ["test_var1", "test_var2"])
-
-    # Define the expected result
     expected = {"type": "test_type",
                 "dataset": "test_dataset",
                 "date": "test_date",
                 "variables": ["test_var1", "test_var2"]
                 }
 
-    # Assert that the result matches the expected result
     assert result == expected
 
 
 def test_receive_response(monkeypatch):
-    # Mock the socket connection
     mock_socket = MagicMock()
     monkeypatch.setattr("socket.socket", lambda *args, **kwargs: mock_socket)
 
-    # Define the expected response
     expected = {"success": True, "response": {"test_var1": {"data": [1, 2, 3]}, "test_var2": {"data": [4, 5, 6]}}}
 
-    # Mock the socket's recv method to return the expected response
     mock_socket.recv.return_value = json.dumps(expected).encode()
-    # Mock the socket's recv method to return the expected response on the first call and an empty bytes object on subsequent calls
+
+    # Mock the socket's recv method to return the expected response on the first call
+    # and an empty bytes object on subsequent calls
     mock_socket.recv.side_effect = [json.dumps(expected).encode(), b'']
     
-    # Call the function
     result = portal.receive_response(mock_socket)
-
-    # Assert that the result matches the expected result
     assert result == expected
 
 
