dc8a0cf8c03acded48ee9072326d0d763b68f5bc
Adam Slay <aslay@mesonet.org>
2023-09-25 14:12:37 -0500

    dynamically set environment/config variables in the Config object

--- accumulator/parm.py
+++
@@ -1,6 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class Parm:
-    parameters: list[tuple[str, str]]  # [(parm, unit), (parm, unit), ...]
--- accumulator/utils/config.py
+++ accumulator/utils/config.py
@@ -1,41 +1,31 @@
-from datetime import datetime, timedelta
-import logging
 import os
-import pytz
 import toml
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
+import pytz
 
 
 class Config:
-    _instance = None
+    def __init__(self):
+        # Load variables from .env file
+        load_dotenv()
 
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super(Config, cls).__new__(cls)
-            cls._instance._initialize()
-        return cls._instance
+        # Set environment variables as attributes
+        for key, value in os.environ.items():
+            setattr(self, key, self._convert_to_int(value))
 
-    def _initialize(self):
-        log = logging.getLogger(__name__)
+        # Load variables from config.toml file
+        config = toml.load('etc/config/config.toml')
+        for key, value in config['settings'].items():
+            setattr(self, key, self._convert_to_int(value))
 
-        # Set the time stamp for the most recent hour(unless current time is within 5 minutes of the top of the hour)
+        # Set the time stamp for the most recent hour
         now_utc = datetime.now(pytz.timezone('UTC'))
         if now_utc.minute <= 5:
             now_utc = now_utc - timedelta(hours=1)
         self.DATE_TIME = now_utc.strftime('%Y-%m-%d %H:00:00')
 
-        # ENVIRONMENT VARIABLES
-        self.ACCUM_DATASET_PATH = os.environ.get('ACCUM_DATASET_PATH', 'default_path')
-        self.DATASERVER_HOST = os.environ.get('DATASERVER_HOST', 'default_ip')
-        self.DATASERVER_PORT = int(os.environ.get('DATASERVER_PORT', 0))
-
-        # CONFIG 
-        config = toml.load('etc/config/config.toml')
-        self.CHILL_HOURS_VAR = config['settings'].get('CHILL_HOURS_VAR', 'chill_hours')
-        self.DATASERVER_DATASET = config['settings'].get('DATASERVER_DATASET', 'edu.ou.mesonet.standard')
-        self.DATASERVER_REQ_TYPE = config['settings'].get('DATASERVER_REQ_TYPE', 'map-netcdf')
-        self.SOCKET_ATTEMPTS = config['settings'].get('SOCKET_ATTEMPTS', 3)
-        self.DATASERVER_TIMEOUT = config['settings'].get('DATASERVER_TIMEOUT', 30)
-        self.MODELS_TO_RUN = config['settings'].get('MODELS_TO_RUN', [])
-        self.STATION_PARAMETERS = config['settings'].get('STATION_PARAMETERS', [])
-        self.LOG_LEVEL = config['settings'].get('LOG_LEVEL', 'INFO')
-        self.LOG_PATH = config['settings'].get('LOG_PATH', 'accumulator.log')
+    @staticmethod
+    def _convert_to_int(value):
+        # If the value is a string and is numeric, convert it to an integer
+        return int(value) if isinstance(value, str) and value.isdigit() else value
