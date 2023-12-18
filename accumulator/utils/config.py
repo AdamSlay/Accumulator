0fc663860153b76fb76e02a2f1aecdb3a9c2f72b
Adam Slay <aslay@mesonet.org>
2023-12-18 14:05:37 -0600

    refactor comments

--- accumulator/utils/config.py
+++ accumulator/utils/config.py
@@ -7,20 +7,16 @@ import pytz
 
 class Config:
     def __init__(self):
-        # Check if .env file exists
         if os.path.exists('.env'):
             load_dotenv()
-        # load example .env file if .env file does not exist(for testing)
         elif os.path.exists('.env.example'):
             load_dotenv('.env.example')
         else:
             raise FileNotFoundError("The .env file was not found.")
 
-        # Set environment variables as attributes
         for key, value in os.environ.items():
             setattr(self, key, self._convert_to_int(value))
 
-        # Check if config.toml file exists
         if os.path.exists('etc/config/config.toml'):
             config = toml.load('etc/config/config.toml')
             for key, value in config['settings'].items():
@@ -28,7 +24,6 @@ class Config:
         else:
             raise FileNotFoundError("The config.toml file was not found.")
 
-        # Set the time stamp for the most recent hour
         now_utc = datetime.now(pytz.timezone('UTC'))
         if now_utc.minute <= 5:
             now_utc = now_utc - timedelta(hours=1)
